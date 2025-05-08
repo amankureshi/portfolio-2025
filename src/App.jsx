b5440484ca39c8e62c7a5ae4624e5feff773d51c
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-09 00:27:25 +0530

    added loader for first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import About from "./Components/About";
 import ContactUs from "./Components/ContactUs";
 import HeroSection from "./Components/HeroSection";
+import Loader from "./Components/Loader";
 import Navbar from "./Components/Navbar";
 import Project from "./Components/Project";
 import Skills from "./Components/Skills";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <>
       <Navbar />
---
+++ src/Components/Loader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="contain">
+      <div className="second"></div>
+      <div className="third"></div>
+      <div className="text">Loading...</div>
+    </div>
+  );
+};
+
+export default Loader;
