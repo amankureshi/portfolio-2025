e73270a208347a6bd94070af862e340a3cb8e07d
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-20 23:10:24 +0530

    added class and two github streck image and git language

--- src/Components/GithubStatic.jsx
+++ src/Components/GithubStatic.jsx
@@ -3,19 +3,32 @@ import "./GithubStatic.css";
 
 const GithubStatic = () => {
   return (
-    <div className="container">
+    <div className="github-section">
       <h2
-        className="section-title text-center mt-5"
+        className="section-title text-center"
         data-aos="fade-up"
         data-aos-anchor-placement="top-bottom"
       >
         My Github Statics
       </h2>
-      <div className="row">
+
+      <div className="contribution-chart">
         <img
           src="https://ghchart.rshah.org/amankureshi"
-          className="github-img "
-          alt=""
+          alt="GitHub Contributions"
+          className="github-img"
+        />
+      </div>
+      <div className="github-cards">
+        <img
+          src="https://streak-stats.demolab.com?user=amankureshi&theme=vision-friendly-dark&card_width=460"
+          alt="GitHub Streak"
+          className="github-card"
+        />
+        <img
+          src="https://github-readme-stats.vercel.app/api/top-langs?username=amankureshi&show_icons=true&locale=en&layout=compact&theme=highcontrast"
+          alt="Top Languages"
+          className="github-card2"
         />
       </div>
     </div>
