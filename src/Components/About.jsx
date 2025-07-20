23a5f92482e58cccad69242addc5cfad9edfa521
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-20 23:28:30 +0530

    update about.jsx and about css

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,23 +12,26 @@ const About = () => {
             <h2 className="section-title" data-aos="fade-up">
               About Me
             </h2>
-            <p className="about-text" data-aos="fade-left">
-              Hi, I'm Aman Kureshi — a passionate Frontend Developer with a
-              completed BCA from KSV University. I love building clean,
-              responsive, and user-friendly web interfaces that make a real
-              impact.
-            </p>
-            <p className="about-text" data-aos="fade-left">
-              I am a Frontend Developer.I love creating beautifull, usefull
-              websites. I am always open to learn new technologies. I always try
-              to expand my tech stack. I also have an extra interest in CSS.
-            </p>
-            <p className="about-text" data-aos="fade-left">
-              I won 1st place in my college's web design competition in my 1st,
-              2nd, and 3rd years. These achievements strengthened my skills,
-              enhanced my creativity, and gave me valuable experience in web
-              development.
-            </p>
+            <ul className="about-text-list">
+              <li className="about-text" data-aos="fade-left">
+                Hi, I'm Aman Kureshi — a passionate Frontend Developer with a
+                completed BCA from KSV University. I love building clean,
+                responsive, and user-friendly web interfaces that make a real
+                impact.
+              </li>
+              <li className="about-text" data-aos="fade-left">
+                I am a Frontend Developer. I love creating beautiful, useful
+                websites. I am always open to learn new technologies. I always
+                try to expand my tech stack. I also have an extra interest in
+                CSS.
+              </li>
+              <li className="about-text" data-aos="fade-left">
+                I won 1st place in my college's web design competition in my
+                1st, 2nd, and 3rd years. These achievements strengthened my
+                skills, enhanced my creativity, and gave me valuable experience
+                in web development.
+              </li>
+            </ul>
           </div>
           <div className="col-md-6">
             <img
