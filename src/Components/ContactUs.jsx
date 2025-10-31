f3844b02e059934a9effcf3218da5f9c8b6f61c1
Aman kureshi <amankureshi78692@gmail.com>
2025-10-31 21:38:46 +0530

    added darkMode in contact.jsx file

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -4,47 +4,70 @@ import { FaMapMarkerAlt, FaCodepen } from "react-icons/fa";
 import { MdCall } from "react-icons/md";
 import { SiMinutemailer } from "react-icons/si";
 
-const ContactUs = () => {
+const ContactUs = ({ darkMode }) => {
   return (
-    <section className="contact-section" id="contact">
+    <section
+      className={`contact-section ${darkMode ? "contact-section-dark" : ""}`}
+      id="contact"
+    >
       <div className="container">
-        <h2 className="section-title text-center" data-aos="fade-up">
+        <h2
+          className={`section-title text-center ${
+            darkMode ? "text-light" : ""
+          }`}
+          data-aos="fade-up"
+        >
           Get in Touch
         </h2>
 
         <div className="contact-info">
-          <div className="info-box">
-            <FaMapMarkerAlt className="icon" />
-            <h5>Address</h5>
-            <p>Kadi, Gujarat, India</p>
-          </div>
-          <div className="info-box">
-            <MdCall className="icon" />
-            <h5>Phone</h5>
-            <p>+91 97128 27603</p>
-          </div>
-          <div className="info-box">
-            <SiMinutemailer className="icon" />
-            <h5>Email</h5>
-            <p>amankureshi78692@gmail.com</p>
-          </div>
-          <div className="info-box">
-            <FaCodepen className="icon" />
-            <h5>CodePen</h5>
-            <p>
-              <a
-                href="https://codepen.io/amankureshi"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                amankureshi
-              </a>
-            </p>
-          </div>
+          {[
+            {
+              icon: <FaMapMarkerAlt className="icon" />,
+              title: "Address",
+              text: "Kadi, Gujarat, India",
+            },
+            {
+              icon: <MdCall className="icon" />,
+              title: "Phone",
+              text: "+91 97128 27603",
+            },
+            {
+              icon: <SiMinutemailer className="icon" />,
+              title: "Email",
+              text: "amankureshi78692@gmail.com",
+            },
+            {
+              icon: <FaCodepen className="icon" />,
+              title: "CodePen",
+              text: (
+                <a
+                  href="https://codepen.io/amankureshi"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  amankureshi
+                </a>
+              ),
+            },
+          ].map((item, i) => (
+            <div
+              key={i}
+              className={`info-box ${darkMode ? "info-box-dark" : ""}`}
+            >
+              {item.icon}
+              <h5>{item.title}</h5>
+              <p>{item.text}</p>
+            </div>
+          ))}
         </div>
 
         <div className="contact-form-wrapper">
-          <div className="form-container">
+          <div
+            className={`form-container ${
+              darkMode ? "form-container-dark" : ""
+            }`}
+          >
             <h4>Send a Message</h4>
             <form>
               <input type="text" placeholder="Full Name" required />
